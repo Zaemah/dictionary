c36ad4524269a7cf49512b4bbd71dd034519182f
Emmah Zanele Mpofu <148703284+Zaemah@users.noreply.github.com>
2023-11-23 17:10:13 -0800

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import Results from "./Results.js";
 import Form from "./Form.js";
-import Image from "./dictionaryIcon.jpg";
+import Image from "./dictionary-icon.jpg";
 export default function Dictionary() {
   const [word, setWord] = useState("");
   const [result, setResult] = useState(null);
 
   return (
     <div className="Dictionary">
-      <h1 className="mb-4">
-        <img src={Image} alt="dictionary icon" className="icon img-fuid"/>
-      </h1>
+       <img src={Image} alt="dictionary icon" className="icon img-fuid"/>
+     <h1 className="mb-4">Dictionery </h1>
       <div className="section">
         <h2 className="mb-3">What word would you like to look up?</h2>
         <Form word={word} setWord={setWord} setResult={setResult} />
